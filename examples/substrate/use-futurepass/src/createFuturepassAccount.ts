0708d473a10b8a752d6baf60b251e1bfc676c01a
Aidan Starke <starke.aidan@gmail.com>
2023-07-06 07:20:07 +1200

    Prettier format

--- examples/substrate/use-futurepass/src/createFuturepassAccount.ts
+++ examples/substrate/use-futurepass/src/createFuturepassAccount.ts
@@ -5,23 +5,23 @@ import { sendExtrinsic } from "@trne/utils/sendExtrinsic";
 import { cleanEnv, str } from "envalid";
 
 const env = cleanEnv(process.env, {
-	CALLER_PRIVATE_KEY: str(), // private key of extrinsic caller
+  CALLER_PRIVATE_KEY: str(), // private key of extrinsic caller
 });
 
 export async function main() {
-	const BOB = "0x25451A4de12dcCc2D166922fA938E900fCc4ED24";
-	const api = await getChainApi("local");
-	const extrinsic = api.tx.futurepass.create(BOB);
-	const caller = createKeyring(env.CALLER_PRIVATE_KEY);
+  const BOB = "0x25451A4de12dcCc2D166922fA938E900fCc4ED24";
+  const api = await getChainApi("local");
+  const extrinsic = api.tx.futurepass.create(BOB);
+  const caller = createKeyring(env.CALLER_PRIVATE_KEY);
 
-	const { result } = await sendExtrinsic(extrinsic, caller, { log: console });
-	const [event] = filterExtrinsicEvents(result.events, [
-		"Futurepass.FuturepassCreated",
-	]);
+  const { result } = await sendExtrinsic(extrinsic, caller, { log: console });
+  const [event] = filterExtrinsicEvents(result.events, [
+    "Futurepass.FuturepassCreated",
+  ]);
 
-	console.log("Extrinsic Result", event.toJSON());
+  console.log("Extrinsic Result", event.toJSON());
 
-	await api.disconnect();
+  await api.disconnect();
 }
 
 main();
