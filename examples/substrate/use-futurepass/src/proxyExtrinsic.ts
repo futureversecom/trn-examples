00e5af7a25acce5c70c27e1c6333c35ed96e5441
Aidan Starke <52016903+aidan-starke@users.noreply.github.com>
2023-07-07 10:14:45 +1200

    [Spike | FuturePass] Add `proxyExtrinsic` example (#5)
    * Add `proxyExtrinsic` example
    
    * Add comments

---
+++ examples/substrate/use-futurepass/src/proxyExtrinsic.ts
@@ -0,0 +1,40 @@
+import { createKeyring } from "@trne/utils/createKeyring";
+import { filterExtrinsicEvents } from "@trne/utils/filterExtrinsicEvents";
+import { getChainApi } from "@trne/utils/getChainApi";
+import { sendExtrinsic } from "@trne/utils/sendExtrinsic";
+import { cleanEnv, str } from "envalid";
+
+const env = cleanEnv(process.env, {
+  CALLER_PRIVATE_KEY: str(), // private key of extrinsic caller
+});
+
+export async function main() {
+  const api = await getChainApi("porcini");
+  const caller = createKeyring(env.CALLER_PRIVATE_KEY);
+
+  const futurepass = (
+    await api.query.futurepass.holders(caller.address)
+  ).toString();
+  // can be any extrinsic, using `system.remarkWithEvent` for simplicity sake
+  const call = api.tx.system.remarkWithEvent("Hello World");
+
+  const extrinsic = api.tx.futurepass.proxyExtrinsic(futurepass, call);
+
+  const { result } = await sendExtrinsic(extrinsic, caller, { log: console });
+  // depending on what extrinsic call you have, filter out the right event here
+  const [remarkEvent] = filterExtrinsicEvents(result.events, [
+    "System.Remarked",
+  ]);
+  const [proxyEvent] = filterExtrinsicEvents(result.events, [
+    "Futurepass.ProxyExecuted",
+  ]);
+
+  console.log("Extrinsic Result", {
+    remark: remarkEvent.toJSON(),
+    proxy: proxyEvent.toJSON(),
+  });
+
+  await api.disconnect();
+}
+
+main();
