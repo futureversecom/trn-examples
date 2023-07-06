7a8b7e263997894ca2e43e6f777387da5a1cdb45
Aidan Starke <starke.aidan@gmail.com>
2023-07-07 08:36:46 +1200

    Use `getAmountsIn`, add comments

--- examples/substrate/use-fee-proxy/src/callWithFeePreferences.ts
+++ examples/substrate/use-fee-proxy/src/callWithFeePreferences.ts
@@ -20,21 +20,22 @@ export async function main() {
   const api = await getChainApi("porcini");
   const caller = createKeyring(env.CALLER_PRIVATE_KEY);
 
+  // can be any extrinsic, using `system.remarkWithEvent` for simplicity
   const call = api.tx.system.remarkWithEvent("Hello World");
   const paymentInfo = await call.paymentInfo(caller.address);
   const estimatedFee = paymentInfo.partialFee.toString();
 
   const { paymentAsset } = argv as unknown as { paymentAsset: number };
 
+  // querying the dex for swap price, to determine the `maxPayment` you are willing to pay
   const {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    Ok: [_, amountOut],
+    Ok: [amountIn],
   } = (
     await // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (api.rpc as any).dex.getAmountsOut(estimatedFee, [XrpAssetId, paymentAsset])
+    (api.rpc as any).dex.getAmountsIn(estimatedFee, [paymentAsset, XrpAssetId])
   ).toJSON();
-  // Allow a buffer to prevent extrinsic failure
-  const maxPayment = Number(amountOut * 1.5).toFixed();
+  // allow a buffer to prevent extrinsic failure
+  const maxPayment = Number(amountIn * 1.5).toFixed();
 
   const extrinsic = api.tx.feeProxy.callWithFeePreferences(
     paymentAsset,
@@ -43,6 +44,7 @@ export async function main() {
   );
 
   const { result } = await sendExtrinsic(extrinsic, caller, { log: console });
+  // depending on what extrinsic call you have, filter out the right event here
   const [remarkEvent] = filterExtrinsicEvents(result.events, [
     "System.Remarked",
   ]);
