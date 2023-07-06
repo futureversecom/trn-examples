b65812331c4dd32a4274ce12995ea36437acf51a
Val Dolot <vldolot@gmail.com>
2023-07-07 09:33:55 +1200

    fix lint issue

--- examples/evm/use-hardhat/test/Counter.js
+++ examples/evm/use-hardhat/test/Counter.js
@@ -1,38 +1,38 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
-  
+
 describe("Counter", function () {
-    async function deployFixture() {
-      const CounterFactory = await ethers.getContractFactory("Counter");
-      const initialCount = 0;
-      const counter = await CounterFactory.deploy(initialCount);
-    
-      return { counter, initialCount };
-    }
-  
-    describe("Deployment", function () {
-      it("Should set the correct initial count", async function () {
-        const { counter, initialCount } = await loadFixture(deployFixture);
-  
-        expect(await counter.getCount()).to.equal(initialCount);
-      });
-    });
-  
-    describe("Interaction", function () {
-      it("Should set increase the current count value by 1", async function () {
-        const { counter } = await loadFixture(deployFixture);
-  
-        await counter.increment();
+  async function deployFixture() {
+    const CounterFactory = await ethers.getContractFactory("Counter");
+    const initialCount = 0;
+    const counter = await CounterFactory.deploy(initialCount);
 
-        expect(await counter.getCount()).to.equal(1);
-      });
-  
-      it("Should set decrease the current count value by 1", async function () {
-        const { counter } = await loadFixture(deployFixture);
-  
-        await counter.decrement();
+    return { counter, initialCount };
+  }
 
-        expect(await counter.getCount()).to.equal(-1);
-      });
+  describe("Deployment", function () {
+    it("Should set the correct initial count", async function () {
+      const { counter, initialCount } = await loadFixture(deployFixture);
+
+      expect(await counter.getCount()).to.equal(initialCount);
     });
+  });
+
+  describe("Interaction", function () {
+    it("Should set increase the current count value by 1", async function () {
+      const { counter } = await loadFixture(deployFixture);
+
+      await counter.increment();
+
+      expect(await counter.getCount()).to.equal(1);
+    });
+
+    it("Should set decrease the current count value by 1", async function () {
+      const { counter } = await loadFixture(deployFixture);
+
+      await counter.decrement();
+
+      expect(await counter.getCount()).to.equal(-1);
+    });
+  });
 });
