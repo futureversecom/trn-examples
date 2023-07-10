947bf9662a9a536e8724ee3cef669fd31f5082a6
Aidan Starke <52016903+aidan-starke@users.noreply.github.com>
2023-07-11 09:21:53 +1200

    [Feat] `useSft` (#7)
    * Add `useSft` examples
    
    * Cleanup
    
    * Give quantity a variable
    
    * Cleanup
    
    * Update `metadataScheme`
    
    * Fix `use-nft/README.md`

--- examples/substrate/use-sft/src/createToken.ts
+++ examples/substrate/use-sft/src/createToken.ts
@@ -1,11 +1,10 @@
 import assert from "assert";
-import { collectArgs } from "@trne/utils/collectArgs";
 import { createKeyring } from "@trne/utils/createKeyring";
 import { filterExtrinsicEvents } from "@trne/utils/filterExtrinsicEvents";
 import { getChainApi } from "@trne/utils/getChainApi";
 import { sendExtrinsic } from "@trne/utils/sendExtrinsic";
 import { cleanEnv, str } from "envalid";
-import { stringToHex } from "@polkadot/util";
+import { collectArgs } from "@trne/utils/collectArgs";
 
 const argv = collectArgs();
 
@@ -19,15 +18,34 @@ export async function main() {
   const api = await getChainApi("porcini");
   const caller = createKeyring(env.CALLER_PRIVATE_KEY);
 
-  const baseUri = stringToHex("https://example.com/token/");
+  const tokenName = "MyToken";
+  const initialIssuance = 10;
+  const maxIssuance = 1000;
+  const tokenOwner = caller.address;
   const { collectionId } = argv as unknown as { collectionId: number };
 
-  const extrinsic = api.tx.nft.transfer(collectionId, baseUri);
+  const extrinsic = api.tx.sft.createToken(
+    collectionId,
+    tokenName,
+    initialIssuance,
+    maxIssuance,
+    tokenOwner
+  );
 
   const { result } = await sendExtrinsic(extrinsic, caller, { log: console });
-  const [event] = filterExtrinsicEvents(result.events, ["Nft.BaseUriSet"]);
+  const [event] = filterExtrinsicEvents(result.events, ["Sft.TokenCreate"]);
 
   console.log("Extrinsic Result", event.toJSON());
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_collectionId, tokenId] = (
+    event.toJSON() as {
+      event: {
+        data: [[number, number]];
+      };
+    }
+  ).event.data[0];
+  console.log("Token ID", tokenId);
 
   await api.disconnect();
 }
