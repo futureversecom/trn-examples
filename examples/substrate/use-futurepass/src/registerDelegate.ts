b42898f72245f5cc4243f923c883bbb5e9ca5323
KarishmaBothara <bothara.karishma@gmail.com>
2023-07-10 10:03:31 +0530

    Fix register delegate example

--- examples/substrate/use-futurepass/src/registerDelegate.ts
+++ examples/substrate/use-futurepass/src/registerDelegate.ts
@@ -9,6 +9,7 @@ import { Wallet, utils as ethers } from "ethers";
 
 const env = cleanEnv(process.env, {
   CALLER_PRIVATE_KEY: str(), // private key of extrinsic caller
+  DELEGATE_PRIVATE_KEY: str(),
 });
 
 enum ProxyType {
@@ -22,15 +23,15 @@ enum ProxyType {
 export async function main() {
   const api = await getChainApi("porcini");
   const caller = createKeyring(env.CALLER_PRIVATE_KEY);
-  const wallet = new Wallet(
-    env.CALLER_PRIVATE_KEY,
+
+  const delegateWallet = new Wallet(
+    env.DELEGATE_PRIVATE_KEY,
     getEthersProvider("porcini")
   );
 
   const proxyType = ProxyType.Any;
   // recommended low number, 75 blocks ~= 5 minutes
   const deadline = (await fetchFinalisedHead(api)) + 75;
-  const delegate = "0x25451A4de12dcCc2D166922fA938E900fCc4ED24";
   const futurepass = (
     await api.query.futurepass.holders(caller.address)
   ).toString();
@@ -38,14 +39,14 @@ export async function main() {
   const message = ethers
     .solidityKeccak256(
       ["address", "address", "uint8", "uint32"],
-      [futurepass, delegate, proxyType, deadline]
+      [futurepass, delegateWallet.address, proxyType, deadline]
     )
     .substring(2);
-  const signature = await wallet.signMessage(message);
+  const signature = await delegateWallet.signMessage(message);
 
   const extrinsic = api.tx.futurepass.registerDelegateWithSignature(
     futurepass, //        Futurepass account to register the account as delegate
-    delegate, //          The delegated account for the futurepass
+    delegateWallet.address, //          The delegated account for the futurepass
     proxyType, //         Delegate permission level
     deadline, //          Deadline for the signature
     signature //          Signature of the message parameters
