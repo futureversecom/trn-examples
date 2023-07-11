a6af9a0e6c50d82feb78ec8a14acbea218ab7591
Aidan <starke.aidan@gmail.com>
2023-07-11 16:02:01 +1200

    Add `addLiquidity` example

--- examples/substrate/use-dex/README.md
+++ examples/substrate/use-dex/README.md
@@ -8,7 +8,7 @@ export CALLER_PRIVATE_KEY=0x000...
 
 ### Swap
 
-Using the `dex.swap(amountIn, amountOutMin, path, to, deadline)` extrinsic
+Using the `dex.swapWithExactSupply(amountIn, amountOutMin, path, to, deadline)` extrinsic
 
 - `amountIn` - Exact supply amount
 - `amountOutMin` - Acceptable minimum target amount
@@ -17,11 +17,34 @@ Using the `dex.swap(amountIn, amountOutMin, path, to, deadline)` extrinsic
 - `deadline` - The deadline of executing this extrinsic. The deadline won't be checked if it is set to None
 
 ```
-api.tx.dex.swap(1_000_000, 500_000, [1, 2], null, null);
+api.tx.dex.swapWithExactSupply(1_000_000, 500_000, [1, 2], null, null);
 ```
 
 Run the command below to execute the example script, ensure you have specified a valid `CALLER_PRIVATE_KEY`
 
 ```
 pnpm call src/swap.ts
+```
+
+### Add Liquidity
+
+Using the `dex.addLiquidity(tokenA, tokenB, amountADesired, amountBDesired, amountAMin, amountBMin, to, deadline)` extrinsic
+
+- `tokenA` - Asset id A
+- `tokenB` - Bsset id B
+- `amountADesired` - Amount A desired to add
+- `amountBDesired` - Bmount B desired to add
+- `amountAMin` - Amount A minimum willing to add
+- `amountBMin` - Bmount B minimum willing to add
+- `to` - The recipient of the swapped token asset. The caller is the default recipient if it is set to None
+- `deadline` - The deadline of executing this extrinsic. The deadline won't be checked if it is set to None
+
+```
+api.tx.dex.addLiquidity(1, 2, 1_000_000, 1_000_000, 0, 0, null, null);
+```
+
+Run the command below to execute the example script, ensure you have specified a valid `CALLER_PRIVATE_KEY`
+
+```
+pnpm call src/addLiquidity.ts
 ```
--- examples/substrate/use-dex/src/addLiquidity.ts
+++ examples/substrate/use-dex/src/addLiquidity.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createKeyring } from "@trne/utils/createKeyring";
 import { filterExtrinsicEvents } from "@trne/utils/filterExtrinsicEvents";
 import { getChainApi } from "@trne/utils/getChainApi";
@@ -15,27 +16,38 @@ export async function main() {
   const api = await getChainApi("porcini");
   const caller = createKeyring(env.CALLER_PRIVATE_KEY);
 
-  const oneXrp = 1_000_000;
+  const tokenA = RootAssetId;
+  const tokenB = XrpAssetId;
+  const amountADesired = 10_000_000;
+  const amountBDesired = 10_000_000;
+  const amountAMin = 0;
 
-  // querying the dex for swap price, to determine the `amountOutMin` you are willing to accept
-  const {
-    Ok: [amountIn, amountOutMin],
-  } = (
-    await // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (api.rpc as any).dex.getAmountsOut(oneXrp, [XrpAssetId, RootAssetId])
+  // querying the dex for quote, to determine the `amountBMin` you are willing to accept
+  const { 0: tokenALiquidity, 1: tokenBLiquidity } = (
+    await (api.rpc as any).dex.getLiquidity(tokenA, tokenB)
+  ).toJSON();
+  const { Ok: amountBMin } = (
+    await (api.rpc as any).dex.quote(
+      amountADesired,
+      tokenALiquidity,
+      tokenBLiquidity
+    )
   ).toJSON();
 
-  const extrinsic = api.tx.dex.swapWithExactSupply(
-    amountIn,
-    amountOutMin,
-    [XrpAssetId, RootAssetId],
+  const extrinsic = api.tx.dex.addLiquidity(
+    tokenA,
+    tokenB,
+    amountADesired,
+    amountBDesired,
+    amountAMin,
+    amountBMin,
     null, // to
     null // deadline
   );
 
   const { result } = await sendExtrinsic(extrinsic, caller, { log: console });
   // depending on what extrinsic call you have, filter out the right event here
-  const [event] = filterExtrinsicEvents(result.events, ["Dex.Swap"]);
+  const [event] = filterExtrinsicEvents(result.events, ["Dex.AddLiquidity"]);
 
   console.log("Extrinsic Result", event.toJSON());
 
