00e91c176cf02f57ca66a7b9591ff3d137602ca4
vldolot <vldolot@gmail.com>
2023-07-12 10:07:08 +1200

    [TRN-192] use-forge examples (#11)
    * use-forge examples
    
    * add install Foundry step on PR check workflow
    
    * add examples/evm/use-forge/lib/ folder

---
+++ examples/evm/use-forge/script/Counter.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "../contracts/Counter.sol";
+
+import "forge-std/console.sol";
+import "forge-std/Script.sol";
+
+contract CounterScript is Script {
+  function setUp() public {}
+
+  function run() public {
+    uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+
+    vm.startBroadcast(deployerPrivateKey);
+    new Counter(0);
+    vm.stopBroadcast();
+  }
+}
