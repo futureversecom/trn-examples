42e324e3b1b257b0988a97669ec24de1a8565267
KarishmaBothara <bothara.karishma@gmail.com>
2023-07-18 16:04:08 +0530

    Change endpoint

--- examples/substrate/use-feeProxy/src/feeProxyWithProxyExtrinsic.ts
+++ examples/substrate/use-feeProxy/src/feeProxyWithProxyExtrinsic.ts
@@ -17,7 +17,7 @@ const env = cleanEnv(process.env, {
 export async function main() {
   assert("paymentAsset" in argv, "Payment asset ID is required");
 
-  const api = await getChainApi("local");
+  const api = await getChainApi("porcini");
   const caller = createKeyring(env.CALLER_PRIVATE_KEY);
   const fpassAddress = (
     await api.query.futurepass.holders(caller.address)
