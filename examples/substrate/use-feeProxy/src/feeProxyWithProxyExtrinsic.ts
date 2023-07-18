ab2da1d9faa7c3f3a4d2d975b7428fb22297c21e
KarishmaBothara <bothara.karishma@gmail.com>
2023-07-18 08:01:36 +0530

    As per review comments

--- examples/substrate/use-feeProxy/src/feeProxyWithProxyExtrinsic.ts
+++ examples/substrate/use-feeProxy/src/feeProxyWithProxyExtrinsic.ts
@@ -9,45 +9,49 @@ import { cleanEnv, str } from "envalid";
 const argv = collectArgs();
 
 const env = cleanEnv(process.env, {
-    CALLER_PRIVATE_KEY: str(), // private key of extrinsic caller
+  CALLER_PRIVATE_KEY: str(), // private key of extrinsic caller
 });
 
-
 // This example assumes, there is liquidity pool for paymentAsset AND XrpAsset (100,000,000,000 & 100,000,000,000)
+// Also assumes, that the futurepassAddress has payment asset.
 export async function main() {
-    assert("paymentAsset" in argv, "Payment asset ID is required");
+  assert("paymentAsset" in argv, "Payment asset ID is required");
 
-    const api = await getChainApi("local");
-    const caller = createKeyring(env.CALLER_PRIVATE_KEY);
-    console.log('********************');
-    // create futurepass account
-    let extrinsic = api.tx.futurepass.create(caller.address)
-    await sendExtrinsic(extrinsic, caller, { log: console });
-    const futurepassAddress = (await api.query.futurepass.holders(caller.address)).toString();
-    const { paymentAsset } = argv as unknown as { paymentAsset: number };
+  const api = await getChainApi("local");
+  const caller = createKeyring(env.CALLER_PRIVATE_KEY);
+  const fpassAddress = (
+    await api.query.futurepass.holders(caller.address)
+  ).toString();
+  const { paymentAsset } = argv as unknown as { paymentAsset: number };
 
-    // mint fee tokens to futurepass
-    extrinsic = api.tx.assets.mint(paymentAsset, futurepassAddress, 2000000000000);
-    await sendExtrinsic(extrinsic, caller, { log: console });
+  // can be any extrinsic, using `system.remarkWithEvent` for simplicity
+  const innerCall = api.tx.system.remarkWithEvent("Hello World");
 
-    // can be any extrinsic, using `system.remarkWithEvent` for simplicity
-    const innerCall = api.tx.system.remarkWithEvent("Hello World");
+  const proxyExtrinsic = api.tx.futurepass.proxyExtrinsic(
+    fpassAddress,
+    innerCall
+  );
+  const maxPayment = 1000000;
+  const feeproxiedCall = api.tx.feeProxy.callWithFeePreferences(
+    paymentAsset,
+    maxPayment,
+    proxyExtrinsic
+  );
+  const { result } = await sendExtrinsic(feeproxiedCall, caller, {
+    log: console,
+  });
 
-    const proxyExtrinsic = api.tx.futurepass.proxyExtrinsic(futurepassAddress, innerCall);
-    const feeproxiedCall = api.tx.feeProxy.callWithFeePreferences(paymentAsset, 1000000, proxyExtrinsic);
-    const { result } = await sendExtrinsic(feeproxiedCall, caller, { log: console });
+  const [proxyEvent, remarkEvent] = filterExtrinsicEvents(result.events, [
+    "FeeProxy.CallWithFeePreferences",
+    // depending on what extrinsic call you have, filter out the right event here
+    "System.Remarked",
+  ]);
+  console.log("Extrinsic Result", {
+    proxy: proxyEvent.toJSON(),
+    remark: remarkEvent.toJSON(),
+  });
 
-    const [proxyEvent, remarkEvent] = filterExtrinsicEvents(result.events, [
-        "FeeProxy.CallWithFeePreferences",
-        // depending on what extrinsic call you have, filter out the right event here
-        "System.Remarked",
-    ]);
-    console.log("Extrinsic Result", {
-        proxy: proxyEvent.toJSON(),
-        remark: remarkEvent.toJSON(),
-    });
-
-    await api.disconnect();
+  await api.disconnect();
 }
 
 main();
