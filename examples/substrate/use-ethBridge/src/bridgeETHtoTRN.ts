0d820e27b09bf29b8bcd751672b0d013e8a703d0
Aidan <starke.aidan@gmail.com>
2023-07-20 10:57:50 +1200

    Add `bridgeETHToTRN` example

---
+++ examples/substrate/use-ethBridge/src/bridgeETHtoTRN.ts
@@ -0,0 +1,86 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { collectArgs } from "@trne/utils/collectArgs";
+import { getChainApi } from "@trne/utils/getChainApi";
+import assert from "assert";
+import { cleanEnv, str } from "envalid";
+import { utils as ethers, getDefaultProvider, Wallet } from "ethers";
+
+import { getBridgeContracts, getERC20Contract } from "./contracts";
+
+const argv = collectArgs();
+const env = cleanEnv(process.env, {
+	CALLER_PRIVATE_KEY: str(), // private key of extrinsic caller
+});
+
+const SyloAsset = {
+	decimals: 18,
+	address: "0x147dE5ddE61c38A5289a398B7bC096DF88E6251F",
+};
+
+const EthAsset = {
+	decimals: 18,
+	address: "0x0000000000000000000000000000000000000000",
+};
+
+export async function main() {
+	assert("asset" in argv, "Asset is required");
+	const { asset } = argv as unknown as { asset: string };
+	const isETH = asset === "ETH";
+
+	const api = await getChainApi("porcini");
+	const provider = getDefaultProvider("goerli");
+	const wallet = new Wallet(env.CALLER_PRIVATE_KEY, provider);
+
+	const syloContract = getERC20Contract(SyloAsset.address, wallet);
+	const { bridgeContract, erc20PegContract } = getBridgeContracts("goerli", wallet);
+
+	const destination = wallet.address;
+	const tokenAddress = isETH ? EthAsset.address : SyloAsset.address;
+	// Bridge requires a small fee
+	const sendMessageFee = ethers.formatEther(await bridgeContract.sendMessageFee());
+	const transferAmount = isETH ? "0.001" : "1";
+	const amount = ethers.parseEther(transferAmount);
+
+	const args = [tokenAddress, amount, destination];
+	const value = ethers.parseEther(
+		isETH ? String(Number(transferAmount) + Number(sendMessageFee)) : sendMessageFee
+	);
+
+	if (!isETH) {
+		const approveTx = await syloContract.approve(erc20PegContract.address, amount);
+		const approveReceipt = await approveTx.wait();
+		console.log("approved", approveReceipt.transactionHash);
+	}
+
+	const gasLimit = await erc20PegContract.estimateGas.deposit(...args, {
+		value,
+	});
+	const tx = await erc20PegContract.deposit(...args, {
+		value,
+		gasLimit,
+	});
+
+	const receipt = await tx.wait();
+	console.log("deposited", receipt.transactionHash);
+
+	// subscribe to system events via storage
+	api.query.system.events((events: any[]) => {
+		console.log(`\nReceived ${events.length} event(s):`);
+
+		// loop through the Vec<EventRecord>
+		events.forEach((record) => {
+			const { event } = record;
+			console.log("event", event.section, event.method);
+			if (event.section === "ethBridge" && event.method === "EventSubmit") {
+				console.log(`ETH Bridge transaction added successfully, for event id ${event.data[0]}`);
+			}
+
+			if (event.section === "ethBridge" && event.method === "ProcessingOk") {
+				console.log(`ETH Bridge transaction executed successfully, for event id ${event.data[0]}`);
+				api.disconnect().then(() => process.exit(0));
+			}
+		});
+	});
+}
+
+main();
