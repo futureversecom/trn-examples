4db4f7c8b8f5dd203c752aa75fdf53580aa38f2f
Ken Vu <ken.vu@futureverse.com>
2023-09-09 22:22:09 +1200

    Add TSDoc for `formatEventData`

--- packages/utils/src/formatEventData.ts
+++ packages/utils/src/formatEventData.ts
@@ -1,13 +1,19 @@
 import { EventRecord } from "@polkadot/types/interfaces";
 
-export function formatEventData(
-	event: EventRecord["event"]
-): Record<string, { type: string; value: unknown }> {
+export type EventData = Record<string, { type: string; value: unknown }>;
+
+/**
+ * Formats raw event data into a structured EventData object.
+ *
+ * @param event - The raw event data to format.
+ * @returns An EventData object containing the formatted event data.
+ */
+export function formatEventData(event: EventRecord["event"]): EventData {
 	const data = event.data.toJSON() as unknown[];
 	const fields = event.meta.fields.toJSON() as { name: string; typeName: string }[];
 
 	return fields.reduce((record, { name, typeName: type }, index) => {
 		record[name] = { value: data[index], type };
 		return record;
-	}, {} as Record<string, { type: string; value: unknown }>);
+	}, {} as EventData);
 }
