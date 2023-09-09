b0cf0ab5137635f3d5603f256c7677e350d593b9
Ken Vu <ken.vu@futureverse.com>
2023-09-09 22:21:50 +1200

    Update `filterExtrinsicEvents` to support complex filter

--- packages/utils/src/filterExtrinsicEvents.ts
+++ packages/utils/src/filterExtrinsicEvents.ts
@@ -2,19 +2,45 @@ import { SubmittableResultValue } from "@polkadot/api/types";
 import { EventRecord } from "@polkadot/types/interfaces";
 import assert from "assert";
 
+import { type EventData, formatEventData } from "./formatEventData";
+
+type EventFilterByName = `${string}.${string}`;
+type EventFilterByData = {
+	name: `${string}.${string}`;
+	key: string;
+	data: EventData["x"];
+};
+
+type EventFilter = EventFilterByName | EventFilterByData;
+
+/**
+ * Filters extrinsic events based on provided event filters.
+ *
+ * This function filters a list of extrinsic events based on the provided event filters,
+ * returning an array of EventRecord objects matching the filter criteria.
+ *
+ * @param events - An array of extrinsic events to filter.
+ * @param eventFilters - An array of EventFilter objects specifying criteria for filtering events.
+ * @returns An array of EventRecord objects that match the filter criteria.
+ * @throws An assertion error if the input events array is falsy.
+ */
 export function filterExtrinsicEvents(
 	events: SubmittableResultValue["events"],
-	eventNames: `${string}.${string}`[]
+	eventFilters: EventFilter[]
 ): EventRecord[] {
 	assert(events);
 
-	return eventNames.map((eventName) => {
+	return eventFilters.map((eventFilter) => {
 		const event = events.find(({ event }) => {
 			const name = `${event.section[0].toUpperCase() + event.section.slice(1)}.${
 				event.method
 			}` as `${string}.${string}`;
 
-			return name === eventName;
+			if (typeof eventFilter === "string") return name === eventFilter;
+
+			if (name !== eventFilter.name) return;
+			const eventData = formatEventData(event);
+			return JSON.stringify(eventData[eventFilter.key]) === JSON.stringify(eventFilter.data);
 		});
 
 		assert(event);
