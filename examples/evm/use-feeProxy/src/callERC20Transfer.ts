41278bec94f85b315684c731381b9517421a5458
Ken Vu <ken.vu@futureverse.com>
2023-10-17 10:04:28 +1300

    Update logging output

--- examples/evm/use-feeProxy/src/callERC20Transfer.ts
+++ examples/evm/use-feeProxy/src/callERC20Transfer.ts
@@ -12,7 +12,8 @@ import { ContractReceipt, utils } from "ethers";
  * Use `feeProxy.callWithFeePreferences` to trigger a `transfer` call of SYLO token, and pay gas
  * in ASTO token.
  *
- * Assumes the caller has some ASTO balance.
+ * Assumes the caller has some ASTO balance to pay for gas and some SYLO balance to demonstrate
+ * the transfer
  */
 withEthersProvider("porcini", async (provider, wallet, logger) => {
 	// contracts creation
@@ -21,7 +22,7 @@ withEthersProvider("porcini", async (provider, wallet, logger) => {
 	const asto = getERC20Contract(ASTO_ASSET_ID).connect(wallet);
 
 	// transfer call
-	const transferAmount = utils.parseUnits("1.0", 18); // 1 SYLO
+	const transferAmount = utils.parseUnits("0.1", 18); // 1 SYLO
 	const transferInput = new utils.Interface(ERC20_ABI).encodeFunctionData("transfer", [
 		ALICE,
 		transferAmount,
@@ -29,7 +30,13 @@ withEthersProvider("porcini", async (provider, wallet, logger) => {
 	const transferEstimate = await sylo.estimateGas.transfer(ALICE, transferAmount);
 
 	logger.info(
-		`prepare a transfer call with destination="${ALICE}", amount="${transferAmount}", and estimateGas="${transferEstimate}"`
+		{
+			parameters: {
+				destination: ALICE,
+				amount: transferAmount,
+			},
+		},
+		`create a transfer call`
 	);
 
 	// retrieve the maxPayment and maxFeePerGas parameters with slippage to be 0.05 (5%)
@@ -40,10 +47,7 @@ withEthersProvider("porcini", async (provider, wallet, logger) => {
 		0.05
 	);
 
-	logger.info(
-		`dispatch a feeProxy call with maxPayment="${maxPayment}", and maxFeePerGas="${maxFeePerGas}"`
-	);
-
+	logger.info(`dispatch transaction from wallet=${wallet.address}`);
 	const tx = await feeProxy.callWithFeePreferences(
 		asto.address,
 		maxPayment,
